b41176e453cba35a3be2678db979f016fb3c2bd2
Sam Askling <samaskling@gmail.com>
2022-12-17 15:32:59 -0600

    Can be played and jump in

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,9 +1,61 @@
 from unogame.card import *
 
+def test_constructor():
+
+    test_card = Card(CardColors.GREEN, CardFaces.REVERSE)
+
+    assert test_card.color == CardColors.GREEN
+
+    assert test_card.face == CardFaces.REVERSE
+
 def test_get_emoji_mention():
-    test_card: Card = Card(CardColors.BLUE, CardFaces.PLUS_TWO)
+
+    test_card = Card(CardColors.BLUE, CardFaces.PLUS_TWO)
     assert test_card.get_emoji_mention() == "<:plus2_blue:736430793815097416>"
 
 def test_get_image_url():
-    test_card: Card = Card(CardColors.GREEN, CardFaces.FIVE)
+    
+    test_card = Card(CardColors.GREEN, CardFaces.FIVE)
     assert test_card.get_image_url() == "https://cdn.discordapp.com/attachments/742986384113008712/742986667438112869/Background_47.png"
+
+def test_can_be_played():
+
+    test_top_card = Card(CardColors.YELLOW, CardFaces.THREE)
+
+    # Playable by color
+    test_card_one = Card(CardColors.YELLOW, CardFaces.ONE)
+    # Playable by number
+    test_card_two = Card(CardColors.BLUE, CardFaces.THREE)
+    # Playable by both
+    test_card_three = Card(CardColors.YELLOW, CardFaces.THREE)
+    # Not playable
+    test_card_four = Card(CardColors.RED, CardFaces.SEVEN)
+
+    assert test_card_one.can_be_played(test_top_card)
+
+    assert test_card_two.can_be_played(test_top_card)
+
+    assert test_card_three.can_be_played(test_top_card)
+
+    assert not test_card_four.can_be_played(test_top_card)
+
+def test_can_be_jumped_in():
+
+    test_top_card = Card(CardColors.YELLOW, CardFaces.THREE)
+
+    # Playable by exact match
+    test_card_one = Card(CardColors.YELLOW, CardFaces.THREE)
+    # Not playable, color doesn't match
+    test_card_two = Card(CardColors.BLUE, CardFaces.THREE)
+    # Not playable, number doesn't match
+    test_card_three = Card(CardColors.YELLOW, CardFaces.SEVEN)
+    # Not playable, neither match
+    test_card_four = Card(CardColors.BLUE, CardFaces.SEVEN)
+
+    assert test_card_one.can_be_jumped_in(test_top_card)
+
+    assert not test_card_two.can_be_jumped_in(test_top_card)
+
+    assert not test_card_three.can_be_jumped_in(test_top_card)
+
+    assert not test_card_four.can_be_jumped_in(test_top_card)
--- unogame/card.py
+++ unogame/card.py
@@ -40,6 +40,47 @@ class Card:
 
         return card_images[image_url]
 
+    def can_be_played(self, other_card: Card) -> bool:
+        """
+        Determines if this card can be played on top of other_card
+
+        Args:
+            other_card (Card): The card on the top of the pile
+
+        Returns:
+            bool: If the card can be played
+        """
+        # If this card is wild, then it can always be played
+        if self.color == CardColors.WILD:
+            return True
+        # If the colors match, then we good
+        if self.color == other_card.color:
+            return True
+        # If the faces match, then we good
+        if self.face == other_card.face:
+            return True
+
+        # Otherwise, they don't match
+        return False
+
+    def can_be_jumped_in(self, other_card: Card) -> bool:
+        """
+        Determines if this card can be jumped in with on top of other_card
+
+        Args:
+            other_card (Card): The card on the top of the pile
+
+        Returns:
+            bool: If the card can be jumped in with
+        """
+
+        # If the colors and faces match, then we good
+        if self.color == other_card.color and self.face == other_card.face:
+            return True
+
+        # Otherwise, they don't match
+        return False
+
 
 
 
