1d719e727353ee71632592074fa33db38d8ecb55
Sam Askling <samaskling@gmail.com>
2023-05-24 22:19:10 -0500

    solved one bug created 5 more, fml quitting coding

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from pathlib import Path # type: ignore (pylance shadow stdlib issues)
 config = dotenv.dotenv_values(Path('storage/.env'))
 
 bot.run(config['TOKEN'])
+def __init__(self):
+    for i in self:
+        if i>self:
+            print("oopsie")
+        else:
+            breakpoint(print("wewweowowow"))
